5e3e4cc9451e24df8959631f218d0449416d968e
MikaAmann <93271414+MikaAmann@users.noreply.github.com>
2025-05-24 22:41:00 +0200

    added more stuff

--- Assets/MyAssets/Gamemode_Memory/Scripts/MemoryGameManager.cs
+++ Assets/MyAssets/Gamemode_Memory/Scripts/MemoryGameManager.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 using Random = UnityEngine.Random;
 
@@ -12,10 +14,15 @@ public class MemoryGameManager : MonoBehaviour
     public Transform buildOrigin;                 
     public Transform hologramSpawnPoint;
     public float hologramTimer;
+    public BuildZone buildZone;
     
     private GameObject currentReferenceTower;
     private GameObject currentHologram;
     private Boolean gameStarted;
+    
+    //[Header("Evaluation")]
+
+    public float matchTolerance = 0.3f;
    
     public void StartGame()
     {
@@ -37,6 +44,60 @@ public class MemoryGameManager : MonoBehaviour
             grab.selectEntered.AddListener(_ => StartCoroutine(HologramHeldTimer()));
             
         }
+        
+    }
+
+    public void Evaluate()
+    {
+        GameObject[] memoryBlocks = GameObject.FindGameObjectsWithTag("Memory");
+        List<GameObject> placedBlocks = buildZone.placedBlocksInZone;
+
+        int correctMatches = 0;
+
+        foreach (GameObject memoryBlock in memoryBlocks)
+        {
+            BlockType memoryType = memoryBlock.GetComponent<BlockType>();
+            GameObject bestMatch = null;
+            float bestDistance = Mathf.Infinity;
+
+            foreach (GameObject placedBlock in placedBlocks)
+            {
+                Debug.Log(placedBlock);
+                
+                BlockType placedType = placedBlock.GetComponent<BlockType>();
+                Debug.Log("Type: " + (memoryType != placedType));
+                if(memoryType != placedType)
+                    continue;
+                
+                float dist = Vector3.Distance(memoryBlock.transform.position, placedBlock.transform.position);
+                Debug.Log(dist);
+                if (dist < matchTolerance && dist < bestDistance)
+                {
+                    bestDistance = dist;
+                    bestMatch = placedBlock;
+                }
+            }
+            
+            if (bestMatch != null)
+            {
+                correctMatches++;
+                placedBlocks.Remove(bestMatch);
+            }
+        }
+        int totalMemoryBlocks = memoryBlocks.Length;
+        int extraBlocks = placedBlocks.Count;
+        
+        float totalScore = correctMatches * 1.0f
+                           + (memoryBlocks.Length - correctMatches) * 0f
+                           + extraBlocks * -0.5f;
+
+        float maxScore = memoryBlocks.Length * 1.0f;
+        float finalScorePercent = Mathf.Clamp01(totalScore / maxScore);
+        
+        Debug.Log("Score: "+finalScorePercent);
+        
+        EndGame();
+        
         
     }
 
